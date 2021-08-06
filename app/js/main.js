cdc20d5e91ec5d27bfc1786d20c74875e42fade5
NetHunter123 <viktordmitryk3@gmail.com>
2021-08-06 19:40:14 +0300

    fixed problems before make task 14

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,18 @@
 $(function(){
-  $('.publicity-slider__inner').slick({
+  $('.publicity__inner').slick({
   });
 
+  var topGoods = document.querySelector('[data-ref="top-goods"]');
+  var goodsOffer = document.querySelector('[data-ref="goods-offer"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(topGoods, config);
+  var mixer2 = mixitup(goodsOffer, config);
 });
 
 
